41830639a4c58df04cdd223418ebf60417be5efd
Sana Sharma <sanasharma2808@gmail.com>
2022-11-17 14:20:53 +0530

    jUnittestingsan

--- src/jUnitTestPackage/jUnitfunctions.java
+++ src/jUnitTestPackage/jUnitfunctions.java
@@ -1,5 +1,12 @@
 package jUnitTestPackage;
 
 public class jUnitfunctions {
+	
+	public int addNumbers(int num1 , int num2) {
+		return num1+num2;
+	}
+	public String addStrings(String strg1 , String strg2) {
+		return strg1+strg2;
+	}
 
 }
